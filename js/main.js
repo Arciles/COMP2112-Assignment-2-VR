c2ee42577ccb8298bb7c68ab0fba68f0d32f4b39
IBIS, Esat <esatibis@expertech.net>
2016-10-13 13:26:28 -0400

    project version 0.6

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by esattahaibis on 2016-10-12.
+ */
+
+/**
+var box = document.querySelector('a-box');
+
+box.addEventListener('mouseenter', function () {
+    box.setAttribute('scale',{
+        x: 5,
+        y: 3,
+        z: 5
+    });
+    box.setAttribute('src', "#txWooden");
+});
+
+var door = document.querySelector('#box-door');
+
+door.addEventListener('click', function(){
+
+});
+*/
